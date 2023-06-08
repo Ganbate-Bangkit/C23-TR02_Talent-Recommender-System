de36164013762fb6171e9c19412b9714580a28b3
Ganbate-Bangkit <134657706+Ganbate-Bangkit@users.noreply.github.com>
2023-06-08 22:47:33 +0700

    Update main.js

--- www/JavaScript/main.js
+++ www/JavaScript/main.js
@@ -1,19 +1,6 @@
 //Define Dataset
 const talentValues = {
-  "https://storage.googleapis.com/bangkit-c23-tr02-pukulenam-dataset/talentPicture/1_Aditya.jpg": [4, 2, 5, 4, 0.5, 0.5, 3, 2, 5, 0],
-  "https://storage.googleapis.com/bangkit-c23-tr02-pukulenam-dataset/talentPicture/2_Andhika%20Mifta%20Alauddin.jpg": [4, 3, 4, 5, 3, 3, 3, 1, 1, 5],
-  "https://storage.googleapis.com/bangkit-c23-tr02-pukulenam-dataset/talentPicture/3_Ni%20Nyoman%20Ayu%20Sintya%20Dewi.jpg": [4, 3, 3, 4, 3, 1, 4, 4, 3, 2],
-  "https://storage.googleapis.com/bangkit-c23-tr02-pukulenam-dataset/talentPicture/4_Dewa%20Bagus%20Trima%20Putra.jpg": [4, 5, 4, 4, 5, 3, 4, 5, 3, 3],
-  "https://storage.googleapis.com/bangkit-c23-tr02-pukulenam-dataset/talentPicture/5_Patma%20Ari%20Ayu%20Kartini.jpg": [4, 4, 4, 4, 4, 3, 4, 4, 4, 3],
-  "https://storage.googleapis.com/bangkit-c23-tr02-pukulenam-dataset/talentPicture/6_Ni%20Komang%20Risa%20Pebriyanthi.jpg": [4, 2, 3, 5, 2, 1, 2, 4.5, 3, 1],
-  "https://storage.googleapis.com/bangkit-c23-tr02-pukulenam-dataset/talentPicture/7_Suci%20Hastika%20Salma'aini.jpg": [5, 5, 4, 5, 3, 2, 4.5, 4.5, 3, 3],
-  "https://storage.googleapis.com/bangkit-c23-tr02-pukulenam-dataset/talentPicture/8_Ni%20Luh%20Santi%20Wahyuni%20.jpg": [4, 2, 3, 5, 2.5, 1, 2, 4, 2, 1],
-  "https://storage.googleapis.com/bangkit-c23-tr02-pukulenam-dataset/talentPicture/9_Andre%20Winata.jpg": [4, 4, 4, 4, 2, 4, 5, 2, 1.5, 3],
-  "https://storage.googleapis.com/bangkit-c23-tr02-pukulenam-dataset/talentPicture/10_Iga%20Narendra%20Pramawijaya.jpg": [4, 1, 4, 3, 1, 3, 5, 4, 2, 3],
-  "https://storage.googleapis.com/bangkit-c23-tr02-pukulenam-dataset/talentPicture/11_Nyoman%20Satiya%20Nanjaya%20Sadha.jpg": [2, 2, 3, 2, 0, 3, 3, 0, 4, 2],
-  "https://storage.googleapis.com/bangkit-c23-tr02-pukulenam-dataset/talentPicture/12_Abiyyu%20Didar%20Haq.jpg": [5, 5, 4, 2, 0, 3, 3, 0, 5, 1],
-  "https://storage.googleapis.com/bangkit-c23-tr02-pukulenam-dataset/talentPicture/13_Arya%20Karna%20Sampalan.jpg": [4, 4, 4, 4, 3, 3, 4, 5, 3, 4],
-  "https://storage.googleapis.com/bangkit-c23-tr02-pukulenam-dataset/talentPicture/14_Visakha%20Vidyadevi%20Wiguna.jpg": [5, 5, 4, 5, 5, 0, 0, 0, 5, 2],
+ .....
 };
 
 //Default Score: Input 0 on the slider
